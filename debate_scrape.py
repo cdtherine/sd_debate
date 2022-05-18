c2944fd82d68da1b08ee4bc1e244e7749ecea81d
cdtherine <43975741+cdtherine@users.noreply.github.com>
2022-05-18 08:43:49 -0500

    Update debate_scrape.py

--- debate_scrape.py
+++ debate_scrape.py
@@ -1,11 +1,7 @@
 import pandas as pd
-from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-import time
 
 driver = webdriver.Chrome()
 driver.get("https://www.tabroom.com/index/results/")
